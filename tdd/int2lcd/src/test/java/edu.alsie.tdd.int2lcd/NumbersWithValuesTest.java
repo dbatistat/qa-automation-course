77cb46e1a21f5b3b9d66b0c7bd109d62bd14388f
David Batista <dbatista.t@gmail.com>
2018-06-15 22:23:46 -0400

    Added fail test for get numbers with height and width values

---
+++ tdd/int2lcd/src/test/java/edu.alsie.tdd.int2lcd/NumbersWithValuesTest.java
@@ -0,0 +1,67 @@
+package edu.alsie.tdd.int2lcd;
+
+import edu.alsie.tdd.int2lcd.exceptions.NumberComponentNotFounException;
+import edu.alsie.tdd.int2lcd.numbercomponent.BlankNumberComponent;
+import edu.alsie.tdd.int2lcd.numbercomponent.CenterLineComponent;
+import edu.alsie.tdd.int2lcd.numbercomponent.LeftLineComponent;
+import edu.alsie.tdd.int2lcd.numbercomponent.NumberComponent;
+import edu.alsie.tdd.int2lcd.numbercomponent.RightLineComponent;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Description.
+ *
+ * @author David
+ * @since 15/06/2018
+ */
+public class NumbersWithValuesTest {
+
+  @Test
+  public void testGetNumberOne() throws NumberComponentNotFounException {
+    NumbersAbstract numbers = new NumbersWithValues(3, 2);
+
+    List<NumberComponent> oneComponents = numbers.getNumber(1);
+
+    Assert.assertEquals(BlankNumberComponent.class, oneComponents.get(0).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(1).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(2).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(3).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(4).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(5).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(6).getClass());
+  }
+
+  @Test
+  public void testGetNumberTwoWidh3Height2() throws NumberComponentNotFounException {
+    NumbersAbstract numbers = new NumbersWithValues(3, 2);
+
+    List<NumberComponent> oneComponents = numbers.getNumber(2);
+
+    Assert.assertEquals(CenterLineComponent.class, oneComponents.get(0).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(1).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(2).getClass());
+    Assert.assertEquals(CenterLineComponent.class, oneComponents.get(3).getClass());
+    Assert.assertEquals(LeftLineComponent.class, oneComponents.get(4).getClass());
+    Assert.assertEquals(LeftLineComponent.class, oneComponents.get(5).getClass());
+    Assert.assertEquals(CenterLineComponent.class, oneComponents.get(6).getClass());
+  }
+
+  @Test
+  public void testGetNumberTwoWidh3Height3() throws NumberComponentNotFounException {
+    NumbersAbstract numbers = new NumbersWithValues(3, 3);
+
+    List<NumberComponent> oneComponents = numbers.getNumber(2);
+
+    Assert.assertEquals(CenterLineComponent.class, oneComponents.get(0).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(1).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(2).getClass());
+    Assert.assertEquals(RightLineComponent.class, oneComponents.get(3).getClass());
+    Assert.assertEquals(CenterLineComponent.class, oneComponents.get(4).getClass());
+    Assert.assertEquals(LeftLineComponent.class, oneComponents.get(5).getClass());
+    Assert.assertEquals(LeftLineComponent.class, oneComponents.get(6).getClass());
+    Assert.assertEquals(LeftLineComponent.class, oneComponents.get(7).getClass());
+    Assert.assertEquals(CenterLineComponent.class, oneComponents.get(8).getClass());
+  }
+}
